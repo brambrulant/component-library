fb434a62c38313905780eb3a74f3015e2d1255c9
${{ github.actor }} <bramschabbink@hotmail.com>
2023-06-22 15:48:51 +0200

    fix buttonheight

--- packages/trunkrs-fe-core/src/Button/utils.ts
+++ packages/trunkrs-fe-core/src/Button/utils.ts
@@ -1,17 +1,17 @@
-import { TrunkrsTheme } from "@brambrulant/trunkrs.apps.theme";
+import { TrunkrsTheme } from '../Theme'
 
 export const getButtonHeight = (
   theme: TrunkrsTheme,
-  size?: "small" | "medium" | "large"
+  size?: 'small' | 'medium' | 'large',
 ) => {
   switch (size) {
-    case "small":
-      return theme.spacing.units(6);
-    case "medium":
-      return theme.spacing.units(8);
-    case "large":
-      return theme.spacing.units(12);
+    case 'small':
+      return theme.spacing.units(6)
+    case 'medium':
+      return theme.spacing.units(8)
+    case 'large':
+      return theme.spacing.units(12)
     default:
-      return theme.spacing.units(8);
+      return theme.spacing.units(8)
   }
-};
+}
